5e584c4b71d0c99be5a4dd7acce4c8b07621200d
Samuel <samuelsoutosilva14@gmail.com>
2023-03-22 10:04:30 -0300

    feat: add installment entity

---
+++ contract-interface/src/model/entities/Installment.java
@@ -0,0 +1,32 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Installment {
+
+    private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private LocalDate date;
+    private Double amount;
+    
+    public Installment() {
+    }
+
+    public Installment(LocalDate date, Double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return date.format(fmt) + " - " + String.format("%.2f", amount);
+    }
+}
