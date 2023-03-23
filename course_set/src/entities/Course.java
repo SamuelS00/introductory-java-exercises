56a8274f1b3d7a16d8bd56199d1505bea5b9e657
Samuel <samuelsoutosilva14@gmail.com>
2023-03-23 10:24:20 -0300

    add course entity

---
+++ course_set/src/entities/Course.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+
+	private String name;
+
+	private Instructor instructor;
+	private Set<Student> students = new HashSet<>();
+
+	public Course(String name, Instructor instructor) {
+		this.name = name;
+		this.instructor = instructor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void enroll(Student student) {
+		students.add(student);
+	}
+}
